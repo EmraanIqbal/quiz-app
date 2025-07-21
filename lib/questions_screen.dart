399afafde9794b257cea749f160ee37367363c66
Your Name <your.email@example.com>
2025-07-21 15:49:22 +0500

    added the custom widget for answer buttons for reuseability

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -13,15 +14,22 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('File Viewer'),
-      ),
-      body: Center(
-        child: Text(
-          'File Name: $fileName',
-          style: TextStyle(fontSize: 24),
-        ),
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "The Question...",
+            style: TextStyle(color: Colors.white),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          AnswerButton(answerText: "Answer 1", onTap: () {}),
+          AnswerButton(answerText: "Answer 2", onTap: () {}),
+          AnswerButton(answerText: "Answer 3", onTap: () {}),
+        ],
       ),
     );
   }
